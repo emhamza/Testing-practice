0ea3e3306c188b3f5902cde4d9b24cd42b68cdc2
emhamza <imumairhamza@gmail.com>
2023-04-04 20:52:17 +0500

    Task-3: simple calculator function with jest unit test

--- function.test.js
+++ function.test.js
@@ -1,9 +1,62 @@
-const reverseString = require('./function');
+const Calculator = require('./function');
 
-describe('reverseString', () => {
-    it('should reverse a string correctly', () => {
-      expect(reverseString('hello')).toBe('olleh');
-      expect(reverseString('racecar')).toBe('racecar');
-      expect(reverseString('12345')).toBe('54321');
+describe('calcultor', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
     });
-  });
+
+    describe('add', () => {
+        test('adds two positive numbers', () => {
+        expect(calculator.add(2, 3)).toBe(5);
+        });
+
+        test('adds a positive and a negative number', () => {
+        expect(calculator.add(2, -3)).toBe(-1);
+        });
+
+        test('adds two negative numbers', () => {
+        expect(calculator.add(-2, -3)).toBe(-5);
+        });
+    });
+    describe('subtract', () => {
+        test('subtract two positive numbers', () => {
+          expect(calculator.subtract(5, 2)).toBe(3);
+        });
+    
+        test('subtract a positive and a negative number', () => {
+          expect(calculator.subtract(5, -2)).toBe(7);
+        });
+    
+        test('subtract two negative numbers', () => {
+          expect(calculator.subtract(-2, -3)).toBe(1);
+        });
+      });
+    describe('multiply', () => {
+        test('subtract two positive numbers', () => {
+          expect(calculator.multiply(5, 2)).toBe(10);
+        });
+    
+        test('multiply a positive and a negative number', () => {
+          expect(calculator.multiply(5, -2)).toBe(-10);
+        });
+    
+        test('multiply two negative numbers', () => {
+          expect(calculator.multiply(-2, -3)).toBe(6);
+        });
+      });
+    describe('divide', () => {
+        test('divide two positive numbers', () => {
+          expect(calculator.divide(10, 2)).toBe(5);
+        });
+    
+        test('divide a positive and a negative number', () => {
+          expect(calculator.divide(10, -2)).toBe(-5);
+        });
+    
+        test('divide two negative numbers', () => {
+          expect(calculator.divide(-10, -2)).toBe(5);
+        });
+    });
+});
